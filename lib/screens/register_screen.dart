1d8cab5dc0d6a6c6ba56426b1bf672fbcc86326b
autorejecttop <autorejecttop@outlook.com>
2025-11-18 09:51:14 +0700

    Added initial screen for register

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -1,4 +1,7 @@
+import 'package:email_validator/email_validator.dart';
 import 'package:flutter/material.dart';
+import 'package:phone_numbers_parser/phone_numbers_parser.dart';
+import 'package:uts/screens/login_screen.dart';
 
 class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
@@ -8,8 +11,213 @@ class RegisterScreen extends StatefulWidget {
 }
 
 class _RegisterScreenState extends State<RegisterScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final _nameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _phoneNumberController = TextEditingController();
+  final _addressController = TextEditingController();
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _passwordConfirmationController = TextEditingController();
+
+  bool _isPasswordObscure = true;
+  bool _isPasswordConfirmationObscure = true;
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Form(
+            key: _formKey,
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            child: Column(
+              children: [
+                TextFormField(
+                  controller: _nameController,
+                  textInputAction: TextInputAction.next,
+                  keyboardType: TextInputType.name,
+                  decoration: InputDecoration(
+                    labelText: 'Nama Lengkap',
+                    hintText: 'Masukkan nama lengkap Anda',
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Nama lengkap tidak boleh kosong!';
+                    }
+
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: _emailController,
+                  textInputAction: TextInputAction.next,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    hintText: 'Masukkan email Anda',
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Email tidak boleh kosong!';
+                    }
+
+                    if (!EmailValidator.validate(value)) {
+                      return 'Email tidak valid!';
+                    }
+
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: _phoneNumberController,
+                  textInputAction: TextInputAction.next,
+                  keyboardType: TextInputType.phone,
+                  decoration: InputDecoration(
+                    labelText: 'Nomor Telepon',
+                    hintText: 'Masukkan nomor telepon Anda',
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Nomor telepon tidak boleh kosong!';
+                    }
+
+                    try {
+                      final parsed = PhoneNumber.parse(
+                        value,
+                        callerCountry: IsoCode.ID,
+                      );
+
+                      if (!parsed.isValid(type: PhoneNumberType.mobile)) {
+                        return "Masukkan nomor telepon yang valid!";
+                      }
+                    } on PhoneNumberException {
+                      return "Masukkan nomor telepon yang valid!";
+                    }
+
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: _addressController,
+                  textInputAction: TextInputAction.next,
+                  keyboardType: TextInputType.streetAddress,
+                  decoration: InputDecoration(
+                    labelText: 'Alamat',
+                    hintText: 'Masukkan alamat Anda',
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Alamat tidak boleh kosong!';
+                    }
+
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: _usernameController,
+                  textInputAction: TextInputAction.next,
+                  keyboardType: TextInputType.name,
+                  decoration: InputDecoration(
+                    labelText: 'Username',
+                    hintText: 'Masukkan username Anda',
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Username tidak boleh kosong!';
+                    }
+
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: _passwordController,
+                  textInputAction: TextInputAction.next,
+                  keyboardType: TextInputType.text,
+                  obscureText: _isPasswordObscure,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    hintText: 'Masukkan password Anda',
+                    suffixIcon: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          _isPasswordObscure = !_isPasswordObscure;
+                        });
+                      },
+                      icon: Icon(
+                        _isPasswordObscure
+                            ? Icons.visibility_off
+                            : Icons.visibility,
+                      ),
+                    ),
+                  ),
+
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Password tidak boleh kosong!';
+                    }
+
+                    if (value.length < 8) {
+                      return 'Password minimal 8 karakter!';
+                    }
+
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: _passwordConfirmationController,
+                  textInputAction: TextInputAction.next,
+                  keyboardType: TextInputType.text,
+                  obscureText: _isPasswordConfirmationObscure,
+                  decoration: InputDecoration(
+                    labelText: 'Konfirmasi Password',
+                    hintText: 'Masukkan konfirmasi password Anda',
+                    suffixIcon: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          _isPasswordConfirmationObscure =
+                              !_isPasswordConfirmationObscure;
+                        });
+                      },
+                      icon: Icon(
+                        _isPasswordConfirmationObscure
+                            ? Icons.visibility_off
+                            : Icons.visibility,
+                      ),
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Konfirmasi password tidak boleh kosong!';
+                    }
+
+                    if (_passwordController.text !=
+                        _passwordConfirmationController.text) {
+                      return 'Password tidak sama!';
+                    }
+
+                    return null;
+                  },
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(builder: (context) => LoginScreen()),
+                      );
+                    }
+                  },
+                  child: Text(
+                    'Register',
+                    style: Theme.of(context).textTheme.labelLarge,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
