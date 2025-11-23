d5acd133c00e077d43484691f2db0ed1e22803b6
autorejecttop <autorejecttop@outlook.com>
2025-11-23 10:39:13 +0700

    Apply auto fix

--- lib/transaction_model.dart
+++ lib/transaction_model.dart
@@ -1,6 +1,4 @@
-import 'dart:developer';
 
-import 'package:flutter/material.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:flutter/foundation.dart';
 import 'package:uts/database_service.dart';
